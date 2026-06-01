22f651508297a4f2eb83d238edfb669c7182b9e0
Bendi156 <toth.peter.bendeguz.2022i@bankitatabanya.hu>
2026-06-01 18:52:45 +0200

    Title átnevezése

--- src/main/java/org/example/proba/GTNApplication.java
+++ src/main/java/org/example/proba/GTNApplication.java
@@ -12,7 +12,7 @@ public class GTNApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GTNApplication.class.getResource("GTN-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Számkitaláló játék");
         stage.setScene(scene);
         stage.show();
     }
